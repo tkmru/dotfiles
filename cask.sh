ab40b2f9122a1a42732cdd666d538fb9b7fdf4eb
tkmru <i.am.tkmru@gmail.com>
2019-04-05 23:46:21 +0900

    add apps

--- cask.sh
+++ cask.sh
@@ -5,6 +5,15 @@
 # Upgrade any already-installed formulae.
 brew cask upgrade
 
+brew cask install alfred
+brew cask install docker
+brew cask install dropbox
 brew cask install google-chrome
 brew cask install google-japanese-ime
+brew cask install skitch
+brew cask install slack
+brew cask install the-unarchiver
 brew cask install visual-studio-code
+
+brew cask cleanup
+
