fb6ab2168c21684dff9d44f156af8386d5ea56b3
tkmru <i.am.tkmru@gmail.com>
2019-04-30 11:20:41 +0900

    fix global gitignore

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# based on https://github.com/akito19/dotfiles/blob/master/init.sh
+
 set -eu
 
 is_setup() {
@@ -8,7 +10,7 @@ is_setup() {
     read -r answer
     case $answer in
       'yes' | 'y') return 0 ;;
-      [nN]o | 'N') return 1 ;;
+      [nN]o | [nN]) return 1 ;;
       *) echo "Try again because you input incorrect letter. Do you setup $1? [y/N]" ;;
     esac
   done
@@ -17,10 +19,17 @@ is_setup() {
 
 if is_setup 'global .gitignore'; then
   if [ "$(uname)" = 'Darwin' ]; then
-    curl -o ~/.gitignore -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore'
+    curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore' >> ~/.gitignore 
   else
-    curl -o ~/.gitignore -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/Linux.gitignore'
+    curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/Linux.gitignore' >> ~/.gitignore
   fi
+
+  echo '' >> ~/.gitignore
+  echo '# VisualStudioCode' >> ~/.gitignore
+  curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/VisualStudioCode.gitignore' >> ~/.gitignore
+  echo '' >> ~/.gitignore
+  echo '# Vim' >> ~/.gitignore
+  curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/Vim.gitignore' >> ~/.gitignore
 fi
 
 if [ "$(uname)" = 'Darwin' ]; then
